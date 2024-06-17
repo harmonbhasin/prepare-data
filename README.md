7382f0b162dcf4c31d1dc403d56b94987cd36a2e
EC2 Default User <ec2-user@ip-172-31-2-212.ec2.internal>
2024-06-17 20:36:01 +0000

    Tested, now going to write readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # prepare-data
+
+Here are steps:
+
+1. Go (NCBI)[https://www.ncbi.nlm.nih.gov/], enter your Bioproject, and go to the dataset. For this tutorial, I will use PRussin 2019, which si PRJNA525405.
+2. Click on "SRA" under 'RElated information' on the right side of the page.
+3. You should see a pop-up above 'Links from BioProject in the center, that says "View results as an expanded interactive table using the RunSelector. Send results to RunSelector". Click on "RunSelector".
+4. You should see a table with all the runs.
+
--- download_fastq.sh
+++ download_fastq.sh
@@ -3,7 +3,7 @@
 # Path to your accession list
 accession_list="accession_list.txt"
 # Path to your s3 bucket where you want your fastq files to end up
-s3_loc="s3://nao-harmon/test/raw"
+s3_loc="s3://nao-harmon/test/raw/"
 # Number of cores to use
 n=4
 
@@ -11,22 +11,30 @@ n=4
 process_accession() {
 
 local accession=$1
-echo -e "\n${accession}\n"
+echo -e "\n Starting ${accession}\n"
 
 # Download the SRA file
+echo "Prefetching SRA file"
 prefetch $accession
 
 # Convert SRA to FASTQ
-fastq-dump --split-3 --gzip $accession && \
-  pv ${accession}_1.fastq.gz | aws s3 cp - $s3_loc/${accession}_1.fastq.gz && \
-  pv ${accession}_2.fastq.gz | aws s3 cp - $s3_loc/${accession}_2.fastq.gz
+echo "Converting SRA to FASTQ"
+fastq-dump --split-3 --gzip $accession 
 
-# Optionally, delete the SRA file to save space
+# Delete the SRA folder to save space
+echo "Deleteing SRA folder"
 rm -rf ./$accession
+
+# Move files to s3 bucket
+echo "Moving forward read to s3 bucket"
+aws s3 cp ${accession}_1.fastq.gz ${s3_loc}/ 
+
+echo "Moving reverse read to s3 bucket"
+aws s3 cp ${accession}_2.fastq.gz ${s3_loc}/ 
+
 }
 
 export -f process_accession
 
 ## Run the process_accession function in parallel for each accession
 parallel -j ${n} process_accession ::: $(cat $accession_list)
-
